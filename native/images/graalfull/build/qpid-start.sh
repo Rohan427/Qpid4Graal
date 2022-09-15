b7ab452423c72c0b6c205129fe6fd5a931b287ab
Paul Allen <pgallen@gmail.com>
2022-09-15 11:01:33 -0600

    BDB Storage type initial test
    Configuration after BDB storage type test.

--- native/images/graalfull/build/qpid-start.sh
+++ native/images/graalfull/build/qpid-start.sh
@@ -2,5 +2,7 @@
 
 cd /usr/local/qpid
 
-/usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st Memory
-#${WHEREAMI}/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build -DQPID_WORK=/var/qpidwork -st Memory
+/usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st BDB
+
+
+#/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build/bin/qpid-server -DPNAME=QPBRKR -DQPID_HOME=/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build -DQPID_WORK=/var/qpidwork -st Memory
