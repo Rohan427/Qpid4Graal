025f67417eb3414d3affa4986d83bdd3200221c0
Paul Allen <pgallen@gmail.com>
2022-09-15 18:23:06 -0600

    JDBC + PostgreSQL
     - JDBC and PostgreSQL database connection configuration added.
     - Updated native image start script to allow arguments for storage type.

--- native/images/graalfull/build/qpid-start.sh
+++ native/images/graalfull/build/qpid-start.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+COMMAND=""
+
 cd /usr/local/qpid
 
-/usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st BDB
+if [[ $# -eq 0 ]];
+then
+    exec /usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st BDB
+else
+    if [[ $1 -eq "Memory" || $1 -eq "BDB" || $1 -eq "JSON" ]];
+    then
+        exec /usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st ${1}
+    elif [[ $1 -eq "JDBC" ]];
+    then
+        exec /usr/local/qpid/bin/qpid-server -graal -DPNAME=QPBRKR -DQPID_HOME=/usr/local/qpid -DQPID_WORK=/var/qpidwork -st $1 -prop "${2}" -prop "${3}" -prop "${4}"
+    fi
+fi
 
-
-#/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build/bin/qpid-server -DPNAME=QPBRKR -DQPID_HOME=/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build -DQPID_WORK=/var/qpidwork -st Memory
+#exec /usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build/bin/qpid-server -DPNAME=QPBRKR -DQPID_HOME=/usr/local/src/qpid/Qpid4Graal/native/images/graalfull/build -DQPID_WORK=/var/qpidwork -st Memory"
